4837fd61c374f2afd5e72168aa1e24dbd5abdbc8
Thibault Marquand <titotix@free.fr>
2018-07-03 13:02:50 +0200

    add notmyidea theme named notmyideacustom

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'Europe/Athens'
 
 DEFAULT_LANG = 'fr'
 
+THEME = 'themes/notmyideacustom'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
